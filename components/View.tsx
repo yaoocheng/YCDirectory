ef9956ba8100482978b1e1d36f6d6df810d72f5c
yaocheng <yaocheng@murphysec.com>
2025-09-16 19:05:04 +0800

    fix: 修复fetch URL中的拼写错误，移除多余的https://前缀

--- components/View.tsx
+++ components/View.tsx
@@ -7,7 +7,7 @@ const View = async ({ postsData }: { postsData: Startup }) => {
 
     after(async () => {
         // 这是你更新数据库的代码
-        await fetch(`https://${process.env.VERCEL_URL || 'http://localhost:3000'}/api/startup-view/${postsData._id}`, {
+        await fetch(`${process.env.VERCEL_URL || 'http://localhost:3000'}/api/startup-view/${postsData._id}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
