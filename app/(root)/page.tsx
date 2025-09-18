702615712db7208266c37ff8ba5b667d5e56dac7
yaocheng <yaocheng@murphysec.com>
2025-09-18 10:29:17 +0800

    生产判断添加url

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,11 +7,11 @@ export default async function Home({
     searchParams: Promise<{ query?: string }>;
 }) {
     const query = (await searchParams).query;
-    
+
     // 通过API请求获取数据，包含搜索查询参数
     const searchQuery = query ? `?query=${encodeURIComponent(query)}` : '';
-    const response = await fetch(`https://yc-directory-yccccc.vercel.app/api/startup-data${searchQuery}`, {
-        cache: 'no-store' // 确保每次都获取最新数据
+    const response = await fetch(`${process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'}/api/startup-data${searchQuery}`, {
+        next: { revalidate: 10 }, // 确保每次都获取最新数据
     });
 
     if (!response.ok) {
